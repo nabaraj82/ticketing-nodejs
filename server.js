add205b4a7957bae97fb9994ff908277368896fa
Nabaraj <nabarajmagar54@gmail.com>
2024-11-12 10:58:01 +0545

    server created

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-const app = require('./app');
+const dotenv = require("dotenv");
+dotenv.config({
+    path: './config.env'
+});
 
+const app = require("./app");
+
+
+const port = process.env.PORT || 4001;
+
+const server = app.listen(port, () => {
+    console.log(`Server has started at http://localhost:${port}`)
+});
